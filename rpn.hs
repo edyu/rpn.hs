cb6147923686cd294317e16123e67c6197e8ffab
Ed Yu <edlyuu@gmail.com>
2014-10-12 23:22:17 -0700

    Generalize binary operators.

--- rpn.hs
+++ rpn.hs
@@ -3,6 +3,7 @@
 type Stack = [Double]
 type Error = String
 type Result = ([Error], Stack)
+type BinOp = (Double -> Double -> Double)
 
 errOp :: Error
 errOp = "not enough operands"
@@ -18,11 +19,14 @@ main = do
 processInput :: Stack -> IO ()
 processInput stack = do
   line <- getLine
-  if line == "q"
-    then do
+  case line of
+    "q" -> do
       putStrLn "goodbye!"
       return ()
-    else do
+    "?" -> do
+      print stack
+      processInput stack
+    _ -> do
       let (errors, newStack) = process ([], stack) $ tokenize line
       if null errors
         then print $ head newStack
@@ -34,16 +38,13 @@ processInput stack = do
               printErrors xs
             tokenize = words
 
-doMath :: ([Error], t) -> (t -> ([Error], Stack)) -> [String] -> Result
-doMath (errs, stack) math = let res = math stack
-                            in  process (fst res ++ errs, snd res)
-
 process :: Result -> [String] -> Result
 process (errs, stack) [] = (errs, stack)
-process stacks ("+":xs) = doMath stacks add2 xs
-process stacks ("-":xs) = doMath stacks sub2 xs
-process stacks ("*":xs) = doMath stacks mul2 xs
-process stacks ("/":xs) = doMath stacks div2 xs
+process stacks ("+":xs) = doMath stacks (+) xs
+process stacks ("-":xs) = doMath stacks (-) xs
+process stacks ("*":xs) = doMath stacks (*) xs
+process (errs, s@(0:_)) ("/":xs) = process (errDiv : errs, s) xs
+process stacks ("/":xs) = doMath stacks (/) xs
 process (err, stack) (x:xs) = case parseNumber x of
                                 ([e], []) -> process (e : err, stack) xs
                                 ([], [n]) -> process (err, n : stack) xs
@@ -53,23 +54,11 @@ process (err, stack) (x:xs) = case parseNumber x of
           [(n, _)] -> ([], [n])
           _ -> ([], [])
 
-add2 :: [Double] -> Result
-add2 [] = ([errOp], [])
-add2 (x:y:xs) = ([], (y + x) : xs)
-add2 xs@(_:_) = ([errOp], xs)
+doMath :: ([Error], Stack) -> BinOp -> [String] -> Result
+doMath (errs, stack) op = let (newErr, newStack) = binaryMath stack op
+                          in  process (newErr ++ errs, newStack)
 
-sub2 :: [Double] -> Result
-sub2 [] = ([errOp], [])
-sub2 (x:y:xs) = ([], (y - x) : xs)
-sub2 xs@(_:_) = ([errOp], xs)
-
-mul2 :: [Double] -> Result
-mul2 [] = ([errOp], [])
-mul2 (x:y:xs) = ([], (y * x) : xs)
-mul2 xs@(_:_) = ([errOp], xs)
-
-div2 :: [Double] -> Result
-div2 [] = ([errOp], [])
-div2 xs@(0:_) = ([errDiv], xs)
-div2 (x:y:xs) = ([], (y / x) : xs)
-div2 xs@(_:_) = ([errOp], xs)
+binaryMath :: Stack -> BinOp -> Result
+binaryMath [] _ = ([errOp], [])
+binaryMath (x:y:xs) op = ([], (y `op` x) : xs)
+binaryMath xs@(_:_) _ = ([errOp], xs)

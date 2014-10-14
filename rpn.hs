06af6ae02c0ca1018f95b8b580086b32ff649a1f
Ed Yu <edlyuu@gmail.com>
2014-10-14 10:53:48 -0700

    Use type class to dispatch depending on the arity of the operator.

--- rpn.hs
+++ rpn.hs
@@ -1,13 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 import Control.Exception (catch)
 
 type Stack = [Double]
 type Error = String
 type Result = ([Error], Stack)
+
+type UnaOp = (Double -> Double)
 type BinOp = (Double -> Double -> Double)
 type ArrOp = ([Double] -> Double)
-type UnaOp = (Double -> Double)
 
 errOp :: Error
 errOp = "not enough operands"
@@ -18,77 +20,78 @@ errNum = "can't parse number"
 
 main :: IO ()
 main = do
-  processInput []
+    processInput []
 
 catchEOF :: IO a -> (IOError -> IO a) -> IO a
 catchEOF = Control.Exception.catch
 
 processInput :: Stack -> IO ()
 processInput stack = do
-  line <- catchEOF getLine (\_ -> return "q")
-  case line of
-    "q" -> do
-      putStrLn "goodbye!"
-      return ()
-    "?" -> do
-      print stack
-      processInput stack
-    _ -> do
-      let (errors, newStack) = process ([], stack) $ tokenize line
-      if null errors
-        then print $ head newStack
-        else printErrors errors
-      processInput newStack
-      where printErrors = mapM_ putStrLn
-            tokenize = words
+    line <- catchEOF getLine (\_ -> return "q")
+    case line of
+        "q" -> do
+            putStrLn "goodbye!"
+            return ()
+        "?" -> do
+            print stack
+            processInput stack
+        _ -> do
+            let (errors, newStack) = process ([], stack) $ tokenize line
+            if null errors
+              then print $ head newStack
+              else printErrors errors
+            processInput newStack
+            where printErrors = mapM_ putStrLn
+                  tokenize    = words
 
 process :: Result -> [String] -> Result
-process stacks [] = stacks
-process stacks ("+":xs) = processBinOp stacks (+) xs
-process stacks ("-":xs) = processBinOp stacks (-) xs
-process stacks ("*":xs) = processBinOp stacks (*) xs
-process (errs, s@(0:_)) ("/":xs) = process (errDiv : errs, s) xs
-process (errs, s@(0:_)) ("%":xs) = process (errDiv : errs, s) xs
-process stacks ("/":xs) = processBinOp stacks (/) xs
-process stacks ("%":xs) = processBinOp stacks (%) xs
-process stacks ("^":xs) = processBinOp stacks (**) xs
-process stacks ("!":xs) = processUnaOp stacks gamma xs
-process stacks ("sum":xs) = processArrOp stacks sum xs
-process (err, stack) (x:xs) = case parseNumber x of
-                                ([e], []) -> process (e : err, stack) xs
-                                ([], [n]) -> process (err, n : stack) xs
-                                _ -> process (err, stack) xs
-  where parseNumber y = case reads y of
-          [] -> ([errNum], [])
-          [(n, _)] -> ([], [n])
-          _ -> ([], [])
+process stacks []                 = stacks
+process stacks ("+":xs)           = calcStack stacks ((+) :: BinOp) xs
+process stacks ("-":xs)           = calcStack stacks ((-) :: BinOp) xs
+process stacks ("*":xs)           = calcStack stacks ((*) :: BinOp) xs
+process (errs, s@(0:_)) ("/":xs)  = process (errDiv : errs, s) xs
+process (errs, s@(0:_)) ("%":xs)  = process (errDiv : errs, s) xs
+process stacks ("/":xs)           = calcStack stacks ((/) :: BinOp) xs
+process stacks ("%":xs)           = calcStack stacks ((%) :: BinOp) xs
+process stacks ("^":xs)           = calcStack stacks ((**) :: BinOp) xs
+process stacks ("!":xs)           = calcStack stacks (gamma :: UnaOp) xs
+process stacks ("sum":xs)         = calcStack stacks (sum :: ArrOp) xs
+process (err, stack) (x:xs)       = case parseNumber x of
+                                    ([e], []) -> process (e : err, stack) xs
+                                    ([], [n]) -> process (err, n : stack) xs
+                                    _         -> process (err, stack) xs
+    where parseNumber y = case reads y of
+                          []        -> ([errNum], [])
+                          [(n, _)]  -> ([], [n])
+                          _         -> ([], [])
 
-processBinOp :: Result -> BinOp -> [String] -> Result
-processBinOp (errs, stack) op = let (newErr, newStack) = binaryMath stack op
-                                in  process (newErr ++ errs, newStack)
-  where binaryMath [] _ = ([errOp], [])
-        binaryMath (x:y:xs) f = ([], (y `f` x) : xs)
-        binaryMath xs@(_:_) _ = ([errOp], xs)
+class Calculable a where
+    calculate :: Stack -> a -> Result
 
-processUnaOp :: Result -> UnaOp -> [String] -> Result
-processUnaOp (errs, stack) op = let (newErr, newStack) = unaryMath stack op
-                                in process (newErr ++ errs, newStack)
-  where unaryMath [] _ = ([errOp], [])
-        unaryMath (x:xs) f = ([], f x : xs)
+instance Calculable UnaOp where
+    calculate [] _     = ([errOp], [])
+    calculate (x:xs) f = ([], f x : xs)
 
-processArrOp :: Result -> ArrOp -> [String] -> Result
-processArrOp (errs, stack) op = let (newErr, newStack) = arrayMath stack op
-                                in process (newErr ++ errs, newStack)
-  where arrayMath xs f = ([], [(f xs)])
+instance Calculable BinOp where
+    calculate [] _       = ([errOp], [])
+    calculate (x:y:xs) f = ([], (y `f` x) : xs)
+    calculate xs@(_:_) _ = ([errOp], xs)
 
-(%) :: Double -> Double -> Double
+instance Calculable ArrOp where
+    calculate xs f = ([], [(f xs)])
+
+calcStack :: Calculable a => Result -> a -> [String] -> Result
+calcStack (errs, stack) op = let (newErr, newStack) = calculate stack op
+                             in  process (newErr ++ errs, newStack)
+
+(%) :: BinOp
 (%) x y = fromIntegral ((toInt x) `mod` (toInt y))
-  where toInt :: Double -> Int
-        toInt = round
+    where toInt :: Double -> Int
+          toInt = round
 
-gamma :: Double -> Double
+gamma :: UnaOp
 gamma x = fromIntegral (factorial (toInt x))
-  where factorial y | y <= 0 = 1
-                    | otherwise = y * factorial (y - 1)
-        toInt :: Double -> Int
-        toInt = floor
+    where factorial y | y <= 0 = 1
+                      | otherwise = y * factorial (y - 1)
+          toInt :: Double -> Int
+          toInt = floor
